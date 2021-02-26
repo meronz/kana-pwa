b4460887992a26d89993c3a25769b9d85c475022
Salvatore Merone <sal.merone95@gmail.com>
2021-02-26 18:46:27 +0100

    Fix hiragana table

--- src/charTable.js
+++ src/charTable.js
@@ -1,87 +1,3 @@
-// ぁ	Hiragana letter small A
-// あ	Hiragana letter A
-// ぃ	Hiragana letter small I
-// い	Hiragana letter I
-// ぅ	Hiragana letter small U
-// う	Hiragana letter U
-// ぇ	Hiragana letter small E
-// え	Hiragana letter E
-// ぉ	Hiragana letter small O
-// お	Hiragana letter O
-// か	Hiragana letter KA
-// が	Hiragana letter GA
-// き	Hiragana letter KI
-// ぎ	Hiragana letter GI
-// く	Hiragana letter KU
-// ぐ	Hiragana letter GU
-// け	Hiragana letter KE
-// げ	Hiragana letter GE
-// こ	Hiragana letter KO
-// ご	Hiragana letter GO
-// さ	Hiragana letter SA
-// ざ	Hiragana letter ZA
-// し	Hiragana letter SI
-// じ	Hiragana letter ZI
-// す	Hiragana letter SU
-// ず	Hiragana letter ZU
-// せ	Hiragana letter SE
-// ぜ	Hiragana letter ZE
-// そ	Hiragana letter SO
-// ぞ	Hiragana letter ZO
-// た	Hiragana letter TA
-// だ	Hiragana letter DA
-// ち	Hiragana letter TI
-// ぢ	Hiragana letter DI
-// っ	Hiragana letter small TU
-// つ	Hiragana letter TU
-// づ	Hiragana letter DU
-// て	Hiragana letter TE
-// で	Hiragana letter DE
-// と	Hiragana letter TO
-// ど	Hiragana letter DO
-// な	Hiragana letter NA
-// に	Hiragana letter NI
-// ぬ	Hiragana letter NU
-// ね	Hiragana letter NE
-// の	Hiragana letter NO
-// は	Hiragana letter HA
-// ば	Hiragana letter BA
-// ぱ	Hiragana letter PA
-// ひ	Hiragana letter HI
-// び	Hiragana letter BI
-// ぴ	Hiragana letter PI
-// ふ	Hiragana letter HU
-// ぶ	Hiragana letter BU
-// ぷ	Hiragana letter PU
-// へ	Hiragana letter HE
-// べ	Hiragana letter BE
-// ぺ	Hiragana letter PE
-// ほ	Hiragana letter HO
-// ぼ	Hiragana letter BO
-// ぽ	Hiragana letter PO
-// ま	Hiragana letter MA
-// み	Hiragana letter MI
-// む	Hiragana letter MU
-// め	Hiragana letter ME
-// も	Hiragana letter MO
-// ゃ	Hiragana letter small YA
-// や	Hiragana letter YA
-// ゅ	Hiragana letter small YU
-// ゆ	Hiragana letter YU
-// ょ	Hiragana letter small YO
-// よ	Hiragana letter YO
-// ら	Hiragana letter RA
-// り	Hiragana letter RI
-// る	Hiragana letter RU
-// れ	Hiragana letter RE
-// ろ	Hiragana letter RO
-// ゎ	Hiragana letter small WA
-// わ	Hiragana letter WA
-// ゐ	Hiragana letter WI
-// ゑ	Hiragana letter WE
-// を	Hiragana letter WO
-// ん	Hiragana letter N
-
 export var hiraganaToRomaji_Table = {
   あ: "A",
   い: "I",
@@ -112,7 +28,7 @@ export var hiraganaToRomaji_Table = {
   だ: "DA",
   ち: "TI",
   ぢ: "DI",
-  つ: "TU",
+  つ: "TSU",
   づ: "DU",
   て: "TE",
   で: "DE",
@@ -189,7 +105,7 @@ export var romajiToHiragana_Table = {
   DA: "だ",
   TI: "ち",
   DI: "ぢ",
-  TU: "つ",
+  TSU: "つ",
   DU: "づ",
   TE: "て",
   DE: "で",
