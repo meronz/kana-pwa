77fe764fbd7a8f545ac82f874f8e54cbbd13681d
Salvatore Merone <sal.merone95@gmail.com>
2021-02-26 20:03:58 +0100

    Start button centered in page.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="container">
-    <div class="d-flex justify-content-center align-items-center my-3">
-      <router-link
-        tag="button"
-        to="/play"
-        class="btn btn-lg btn-primary"
-        v-t="'app.start'"
-      />
+  <div class="container-fluid min-vh-100 d-flex flex-column">
+    <div class="row flex-grow-1">
+      <div class="row align-items-center">
+        <div class="d-flex justify-content-center align-items-center">
+          <router-link
+            tag="button"
+            to="/play"
+            class="btn btn-lg btn-primary big-button p-3"
+            v-t="'app.start'"
+          />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -14,3 +18,10 @@
 <script>
 export default { name: "Home" };
 </script>
+
+<style scoped>
+.big-button {
+  min-width: 500px;
+  min-width: 50%;
+}
+</style>
